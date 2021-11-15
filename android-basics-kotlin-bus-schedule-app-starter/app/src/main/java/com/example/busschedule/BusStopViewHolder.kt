9f73c3f3c66050fde9c627f5da548fb561269368
eduardo <eduardojose13579@gmail.com>
2021-11-15 11:24:13 -0600

    unidad5

---
+++ android-basics-kotlin-bus-schedule-app-starter/app/src/main/java/com/example/busschedule/BusStopViewHolder.kt
@@ -0,0 +1,20 @@
+package com.example.busschedule
+
+import android.annotation.SuppressLint
+import androidx.recyclerview.widget.RecyclerView
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.databinding.BusStopItemBinding
+import java.sql.Date
+import java.text.SimpleDateFormat
+
+class BusStopViewHolder(private var binding: BusStopItemBinding): RecyclerView.ViewHolder(binding.root) {
+    @SuppressLint("SimpleDateFormat")
+    fun bind(schedule: Schedule) {
+        binding.stopNameTextView.text = schedule.stopName
+        binding.arrivalTimeTextView.text = SimpleDateFormat(
+            "h:mm a").format(
+            Date(schedule.arrivalTime.toLong() * 1000)
+        )
+    }
+
+}
